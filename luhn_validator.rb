a61d4fa33434285cf88c81553c1243f161571c31
chenlizhan <chung1350@hotmail.com>
2015-04-08 21:19:21 +0800

    Copy/Paste the assignmentA, B into this repo

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,19 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
     # TODO: use the integers in nums_a to validate its last check digit
+    nums_a_reverse = nums_a.reverse
+    nums_a_process = nums_a_reverse.each_with_index.map do |num, index|
+      if index.odd?
+        num = num.to_i * 2
+        num.to_s.chars.map(&:to_i).reduce(:+)
+      else 
+        num.to_i
+      end
+    end
+
+    checksum = nums_a_process.shift
+    sum_of_digits = nums_a_process.reduce(:+)
+    sum_of_digits * 9 % 10 == checksum
   end
 end
--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,12 +1,17 @@
 module SubstitutionCipher
-  module Caesar
+  module Caeser
     # Encrypts document using key
     # Arguments:
     #   document: String
     #   key: Fixnum (integer)
     # Returns: String
     def self.encrypt(document, key)
-      # TODO: encrypt string using caesar cipher
+      # TODO: encrypt string using caeser cipher
+      cipher_text = document.to_s.chars.map do |char|
+        char.ord + key < 95 ? (char.ord + key).chr : (char.ord + key - 95).chr
+      end.join
+
+      cipher_text
     end
 
     # Decrypts String document using integer key
@@ -15,7 +20,12 @@ module SubstitutionCipher
     #   key: Fixnum (integer)
     # Returns: String
     def self.decrypt(document, key)
-      # TODO: decrypt string using caesar cipher
+      # TODO: decrypt string using caeser cipher
+      plaint_text = document.to_s.chars.map do |char|
+        char.ord - key >= 32 ? (char.ord - key).chr : (95 + char.ord - key).chr
+      end.join
+
+      plaint_text
     end
   end
 
@@ -26,7 +36,14 @@ module SubstitutionCipher
     #   key: Fixnum (integer)
     # Returns: String
     def self.encrypt(document, key)
-      # TODO: encrypt string using a permutation cipher
+      # TODO: encrypt string using caeser cipher
+      origin = (0..127).to_a
+      cipher = (0..127).to_a.shuffle(random: Random.new(key))
+      result_en = document.to_s.chars.map do |char|
+        cipher[char.ord].chr
+      end.join
+
+      result_en
     end
 
     # Decrypts String document using integer key
@@ -35,7 +52,14 @@ module SubstitutionCipher
     #   key: Fixnum (integer)
     # Returns: String
     def self.decrypt(document, key)
-      # TODO: decrypt string using a permutation cipher
+      # TODO: decrypt string using caeser cipher
+      origin = (0..127).to_a
+      cipher = (0..127).to_a.shuffle(random: Random.new(key))
+      result_de = document.to_s.chars.map do |char|
+        origin[cipher.index(char.ord)].chr
+      end.join
+
+      result_de
     end
   end
 end
